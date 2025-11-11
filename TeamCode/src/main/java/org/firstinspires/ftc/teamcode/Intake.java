fc35b74cc2b29ff6cbac9a42d5060b66f69be1fd
Romtzukdev <romtzukdev@gmail.com>
2025-11-11 14:21:19 +0200

    za

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Intake {
+    private DcMotor intake;
+    public Intake(HardwareMap hardwareMap){
+        this.intake = hardwareMap.get(DcMotor.class,"intake");
+    }
+
+    public Action Take(double power,double time_in_ms){
+        return new Action() {
+            ElapsedTime timer = new ElapsedTime();
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                while(timer.milliseconds() < time_in_ms){
+                    intake.setPower(power);
+                    return true;
+                }
+                return false;
+            }
+        };
+    }
+}
