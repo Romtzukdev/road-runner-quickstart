31baa52a7d62c1bda2e883200bb3b18b52d19eb4
Ellad Win <az4974@eh.amalnet.k12.il>
2025-11-11 14:30:20 +0200

    Meeting Preps
    * Imported Scripts From My Fork
    * Created "FullRobotControl.java"

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/LimelightTest.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+//Ah Yes. Copy-Pasted Code. My Favorite (because i don't need to use my brain)
+//https://docs.limelightvision.io/docs/docs-limelight/apis/ftc-programming
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
+
+@TeleOp(name = "Limelight Test", group = "Test Stuff")
+public class LimelightTest extends LinearOpMode {
+
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    //"Basic" Results (idk what these mean but i think it's important)
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+
+                    //AprilTag Results (oh yeah the good shit)
+                    List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                    for (LLResultTypes.FiducialResult fr /*hehe lol*/ : fiducialResults) {
+                        telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+                    }
+
+                }
+            }
+            telemetry.update();
+        }
+    }
+}
