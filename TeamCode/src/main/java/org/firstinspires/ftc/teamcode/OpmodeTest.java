f580141eaf17c789b2a1c96aa0bee297105ccd48
Romtzukdev <romtzukdev@gmail.com>
2026-01-09 15:34:59 +0200

    Update OpmodeTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpmodeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpmodeTest.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 @TeleOp
 public class OpmodeTest extends OpMode {
